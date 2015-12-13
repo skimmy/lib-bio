1ecbc8eadb54e364e1ab46f0111d4fd558e90998
skimmy <michele.schimd@gmail.com>
2015-12-13 10:43:14 +0100

    Modified sims.sh to work on ranging coverage with run100 script

--- src/simulator/util/sims.sh
+++ src/simulator/util/sims.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
 rm -r results.txt
-#for M in `seq 500 25 1000`
-for M in `seq 1 200`
+
+N=$1
+m=$2
+e=0
+coverage=`seq 1 20`
+
+for c in `seq 1 15`
 do
-    #./simulator.out -N 10000 -m 100 -M $M -e 0 | grep -i fail >> results.txt
-    ./simulator.out -N 10000 -m 100 -M 1000 -e 0 | grep -i fail >> results.txt
+    M=$(((N*c)/m))
+    ./run100.sh $N $M $m $e
+    mean=`python mean.py results100.txt`
+    echo -e "${c}\t${mean}" >> results.txt
 done
+
+
+#for M in `seq 500 25 1000`
+#for M in `seq 1 200`
+#do
+    #./simulator.out -N 10000 -m 100 -M $M -e 0 | grep -i fail >> results.txt
+#    ./simulator.out -N 10000 -m 100 -M 1000 -e 0 | grep -i fail >> results.txt
+#done

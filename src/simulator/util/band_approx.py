104724e52b3153123c71ac35da5f4f3306d5fb04
Michele Schimd <skimmoz@gmail.com>
2017-01-12 10:42:07 +0100

    Added operation partions statistics to band_approx.py

--- src/simulator/util/band_approx.py
+++ src/simulator/util/band_approx.py
@@ -9,6 +9,7 @@ def parseArguments():
     parser.add_argument("file", help="File with results of simulation")
     parser.add_argument("n", help="length of the strings")
     parser.add_argument("-o", dest="outfile", help="Name of the output file", default="out.txt")
+    parser.add_argument("-P","--partition", dest="partition",help="Name of the file for operation distribution", default=None)
     # parser.add_argument("opt", help="Required option")
     # parser.add_argument("-o", "--optional", dest='o', help="Optional flag", action='store_true')
     # parser.add_argument("-d", "--default", help="SWith default", default="Hello")
@@ -33,6 +34,23 @@ def printStatistics(params, Ts, n):
     for v in vals:
         print("{0}\t{1}\t{2}\t{3}".format(v, params[v][0], math.sqrt(params[v][1]), params[v][2]))
 
+def printOperationsStatistics(part, Ts):
+    vals = [T for T in Ts]
+    list.sort(vals)
+    for T in vals:
+        meanS = 0
+        meanD = 0
+        meanI = 0
+        for x in range(len(part[T]['S'])):
+            meanS += x * part[T]['S'][x]
+            meanI += x * part[T]['D'][x]
+            meanD += x * part[T]['I'][x]
+        meanS /= sum(part[T]['S'])
+        meanD /= sum(part[T]['D'])
+        meanI /= sum(part[T]['I'])
+        print("{0}\t{1}\t{2}\t{3}".format(T,meanS, meanD, meanI))
+    
+
 def saveDistributionsToFile(dists, filename, n):
     Ts = [T for T in dists if T < n]
     list.sort(Ts)
@@ -45,7 +63,38 @@ def saveDistributionsToFile(dists, filename, n):
         s += "\n"
         fh.write(s)
     fh.close()
-        
+
+def saveOperationsPartitionToFile(part, Ts, fileName):
+    fh = open(fileName, "w")
+    for T in Ts:
+        pass
+    fh.close()
+
+
+'''Returns a dictionary containing an entry for each of the value of Ts. Each of
+such entry contains a dictionary with 3 entries indexed by 'S', 'D' and 'I' and
+containing (as value) a numpy vector V of size n such that V[i] contains the number
+of times of substitution, deletion and insertion (respectively) observed.
+
+Calling P the returned dictionary the element
+  P[T]['S'][i]
+is the number of times 'i' substitution ('S') where observed in all the scripts
+obtained using the approximation value T
+'''
+def computeOperationsPartition(data, Ts, n):
+    part = {}
+    for T in Ts:
+        part[T] = {}
+        part[T]['S'] = np.zeros(n)
+        part[T]['D'] = np.zeros(n)
+        part[T]['I'] = np.zeros(n)
+        for d in data:
+            part[T]['S'][(d[T][0])] += 1
+            part[T]['D'][(d[T][1])] += 1
+            part[T]['I'][(d[T][2])] += 1
+    return part
+
+
         
 '''Returns a dictionary with a numpy vector for each of the entries
 of Ts. Such vector will contain in position 'i' the number of times
@@ -95,5 +144,10 @@ if __name__ == "__main__":
     dists = computeDistributions(data, Ts, n)
     saveDistributionsToFile(dists, args.outfile, n)
     params = computeStatistics(dists, Ts, n)
-    printStatistics(params,Ts, n)
+    printStatistics(params,Ts, n)    
+    if args.partition != None:
+        print(Ts)
+        part = computeOperationsPartition(data, Ts, n)
+        saveOperationsPartitionToFile(part, Ts, args.partition)
+        printOperationsStatistics(part, Ts)
     fh.close()

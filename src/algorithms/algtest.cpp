0708bcfa69576baa904177859fe5aa64af0e4042
Michele Schimd <skimmoz@gmail.com>
2017-02-13 20:54:02 +0100

    Added common types.h and minor changes

--- src/algorithms/algtest.cpp
+++ src/algorithms/algtest.cpp
@@ -1,5 +1,23 @@
+// algtest.cpp
+
+// Copyright 2017 Michele Schimd
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
 #include "../algorithms.h"
 
+#include <lbio.h>
 #include <algorithms/dynamic_programming.hpp>
 
 #include <iostream>
@@ -23,8 +41,8 @@ int main(int argc, char** argv) {
 
   std::cout << "\n\n" << DNAAlphabet2Bits::charToInt('A') << "\n";
 
-  size_t n = 8;
-  size_t m = 8;
+  lbio_size_t n = 8;
+  lbio_size_t m = 8;
   lbio::dp_matrix<int> matrix {n,m};
   for (int i = 0; i < matrix._rows; ++i) {
     for (int j = 0; j < matrix._cols; ++j) {

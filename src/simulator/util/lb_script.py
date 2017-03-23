6ebf836f090445d3645306f80e1a6644c938404c
Michele Schimd <skimmoz@gmail.com>
2017-03-23 09:47:29 +0100

    Checkpoint for lb_script.py

--- src/simulator/util/lb_script.py
+++ src/simulator/util/lb_script.py
@@ -1,3 +1,4 @@
+import sys
 import math
 import scipy.special as spec
 
@@ -50,7 +51,7 @@ def compute_fn(n, r, D, q, delta_s, delta_e, Sigma):
     # diagonal at the end
     else:
         # ending del
-        d_bar = min(q-1,d)
+        d_bar = min(q-1,D)
         for d in range(d_bar):
             count += (spec.binom(q-1,d) * spec.binom(D-1,d)
                       * spec.binom(D-1, q-d-1))
@@ -61,63 +62,11 @@ def compute_fn(n, r, D, q, delta_s, delta_e, Sigma):
         for i in range(i_bar):
             part_count = 0
             for j in range(1,D-i):
-                part_count += (spec.binom(D-j-1,i-1) * spec.binom(D-1,q-i) *
+                part_count += (spec.binom(D-j-1,i) * spec.binom(D-1,q-i-1) *
                                pow(Sigma,j) * pow(Sigma-1,D-j))
             count += part_count * spec.binom(q-1,i)
             
     return count
-
-##############################################################################
-#                                                                            #
-#                          OLD BOUND FUNCTIONS                               #
-#                                                                            #
-##############################################################################
-
-'''This function computes the number of scripts that can be generated
-through a sufficient path for a given number of non diagonal path q
-and number of Deletion/Insertion D'''
-def compute_sufficient_path_D(n, r, D, q, Sigma):
-    fd00 = compute_fd00(n, r, D, q, Sigma)
-    fd01 = compute_fd01(n, r, D, q, Sigma)
-    fd10 = compute_fd10(n, r, D, q, Sigma)
-    fd11 = compute_fd11(n, r, D, q, Sigma)
-    return fd00
-
-
-'''Compute the volume V_r under the constraint that exactly D
-Deletion and Insertion operation are used'''
-def compute_volume_r_D(n, r, D, Sigma):
-    # Number of matches (n-r+D)
-    M = n - r + D
-    q_max = min(2*D, M+1)
-    Q_sum = 0
-    # q = 2,3,...,min(2D,M+1)
-    for q in range(2,q_max):
-        Q_sum += compute_sufficient_path_D(n, r, D, q, Sigma)
-    return Q_sum
-
-
-'''For a given number of errors  0 <= r <= n computes an upper bound
-V_r to the number of strings at distance <= r from any center A'''
-def compute_volume_r_UB(n, r, Sigma):
-    D_max = r >> 1
-    Vr = computeQ0(n, r, Sigma)
-    for D in range(D_max+1):
-        Vr += compute_volume_r_D(n, r, D, Sigma)
-    return Vr 
-
-'''Computes the lower bound on constant alpha for given n'''
-def compute_lower_bound(n, Sigma=4):
-    norm_const = float(pow(Sigma, n) * n)
-    # The lower bound is obtained via an upper bound on the volume
-    volumes_sum = 0
-    # any value in [1,n-1] contributes to the bound (see LaTeX)
-    for r in range(1,n):
-        vr = compute_volume_r_UB(n, r, Sigma)
-        volumes_sum += vr
-
-        # consider moving normalization inside the above for loop
-    return 1 - (float(volumes_sum) / norm_const)
 
 ##############################################################################
 #                                                                            #
@@ -128,8 +77,8 @@ def compute_lower_bound(n, Sigma=4):
 def count_canonical_annotated_path_r_D_q(n, r, D, q, Sigma):
     count = 0
     # sum of fd*fn
-    for delta_s in range(1):
-        for delta_e in range(1):
+    for delta_s in range(2):
+        for delta_e in range(2):
             fd = compute_fd(n, r, D, q, delta_s, delta_e, Sigma)
             fn = compute_fn(n, r, D, q, delta_s, delta_e, Sigma)
             count += fd*fn
@@ -153,9 +102,12 @@ def count_canonical_annotated_path_r(n, r, Sigma):
 
 def lower_bound(n, Sigma=4):
     lb = 1
+    up_r = []
     for r in range(1,n+1):
-        lb += r * count_canonical_annotated_path_r(n, r, Sigma)
-    return lb / pow(Sigma, 2*n)
+        count = count_canonical_annotated_path_r(n, r, Sigma)
+        up_r.append((r,count))
+        lb += r * count
+    return (lb / pow(Sigma, 2*n), up_r)
 
 ##############################################################################
 #                                                                            #
@@ -164,7 +116,14 @@ def lower_bound(n, Sigma=4):
 ##############################################################################
 
 if __name__ == "__main__":
-    n_max = 64
+    verbosity = 0
+    if (len(sys.argv) > 1):
+        verbosity = int(sys.argv[1])
+    
+    n_max = 16
     for n in range(1,n_max+1):
-        #print("{0}\t{1}".format(n,compute_lower_bound(n)))
-        print("{0}\t{1}".format(n, lower_bound(n)))
+        (lb, lb_r)  = lower_bound(n)
+        if (verbosity > 0):
+            for entry in lb_r:
+                print("  {0}\t{1}".format(entry[0],entry[1]))
+        print("{0}\t{1}\n".format(n, lb))

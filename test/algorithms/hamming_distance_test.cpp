fb851948d4050d6a4ec43d2e7e5cdfd975782998
Michele Schimd <skimmoz@gmail.com>
2017-04-17 11:32:35 +0200

    Started testing suites

---
+++ test/algorithms/hamming_distance_test.cpp
@@ -0,0 +1,30 @@
+// hamming_distance_test.cpp
+
+// Copyright 2017 Michele Schimd
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifdef HAVE_BOOST
+
+#define BOOST_TEST_MODULE hamming_distance_test
+#include <boost/test/included/unit_test.hpp>
+using namespace boost::unit_test;
+
+#include "../../include/algorithms/hamming_distance.hpp"
+
+void free_test_function()
+{
+  BOOST_TEST( true /* test assertion */ );
+}
+
+#endif

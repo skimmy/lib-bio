92165f15aec659bf0070ee5955a8f4563b554669
Michele Schimd <skimmoz@gmail.com>
2017-05-04 14:39:24 +0200

    Added options to sample paths script

--- src/simulator/util/lb_path_gen.py
+++ src/simulator/util/lb_path_gen.py
@@ -1,6 +1,19 @@
 import random
 import re
 import sys
+import argparse
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    # parser.add_argument("opt", help="Required option")
+    parser.add_argument("-p", "--pipeline", help="When set only shows result ratio", action='store_true')
+    parser.add_argument("-n", "--length", help="Length of the strings n", default=16)
+    parser.add_argument("-r", "--errors", help="Total number of errors r", default=8)
+    parser.add_argument("-D", "--deletions", help="Number of insertions and deltions D", default=4)
+    parser.add_argument("-k", "--samples", help="Number of samples k", default=32)
+
+    return parser.parse_args()
+
 
 def generate_random_string(n, r, D):
     script = ['M']*(n+D)
@@ -20,13 +33,12 @@ def generate_random_string(n, r, D):
     return script
 
 
-if (__name__ == "__main__"):    
-    n = 12
-    r = 5
-    D = 2
-    k = 5
-    if (len(sys.argv) > 1):
-        k = int(sys.argv[1])
+if (__name__ == "__main__"):
+    args = parse_arguments()
+    n = int(args.length)
+    r = int(args.errors)
+    D = int(args.deletions)
+    k = int(args.samples)
     do_print = (sys.argv.count("-print"))
 
     # construct Regex for canonical path
@@ -36,10 +48,8 @@ if (__name__ == "__main__"):
         path = "".join(generate_random_string(n,r,D))
         if (re.match(pattern, path)):
             matched += 1
-            if (do_print):
-                print("+ {0}".format(path))
-        else:
-            if(do_print):
-                print("- {0}".format(path))
 
-    print("Mathced {0}/{1} Ratio: {2}".format(matched, k, matched/k))
+    if (args.pipeline):
+        print(matched/k)
+    else:
+        print("Mathced {0}/{1} Ratio: {2}".format(matched, k, matched/k))

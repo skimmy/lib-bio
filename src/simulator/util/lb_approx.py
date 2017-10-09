89a982d5a018f057563d1fc1cc5fefd2217b0047
Michele Schimd <skimmoz@gmail.com>
2017-10-09 18:31:09 +0200

    Canonical path approximation script

--- src/simulator/util/lb_approx.py
+++ src/simulator/util/lb_approx.py
@@ -48,7 +48,41 @@ def bound(n, Sigma=4): # OK
     
     return (lb, hulls_bound, r-1)
 
+def prototype(n_max=32):
+    import math
+    import scipy.special as spec
+    print("TEST")
+    n = 24
+    r = 16
+    Sigma=4
+    rho = 8 / (Sigma-1)
+    x = []
+    y = []
+    for nD in range(1,int(math.floor(r/2))+1):
+        s_power = pow(rho, nD)
+        s_binom_1 = spec.binom(n-nD, n-r+nD)
+        s_binom_2 = spec.binom(n-r+3*nD, n-r+nD)
+        #s = 1*s_binom_1 * 1*s_binom_2
+        #s = 1*s_power *  1*s_binom_2
+        # s = 1*s_power * 1*s_binom_1
+        #s = 1*s_power * 1*s_binom_1 * 1*s_binom_2
+        #s = 1*s_power
+        #s = 1*s_binom_1
+        s = 1*s_binom_2
+        i = n - r + nD
+        a = pow(rho,nD) * pow(math.e*math.e,i)*pow((n-r+3*nD)*(n-nD),i) / pow(i,i)
+        x.append(s)
+        y.append(a)
+    print(x)
+    print(y)
+    print(sum(x))
+    print(sum(y))
+
+
 if (__name__ == "__main__"):
+    if (sys.argv.count("--test") > 0):
+        prototype()
+        sys.exit(0)
     n_max = int(sys.argv[1])
     n_step = int(sys.argv[2])
     Sigma=4
